ff8b0d72267124929cc90e2af0ada2e628fad6a5
Kievina <56592501+Kievina@users.noreply.github.com>
2019-10-28 14:43:14 -0400

    complete part4 - all tests pass

--- src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
+++ src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
@@ -5,7 +5,17 @@ public class Jumper {
     /*
      * Complete the function below.
      */
-    public int jumps(int k, int j) {
-        return -1;
+    public int jumps(int flagHeight, int jumpHeight) {
+        int minNumberOfJumps;
+        int numJumpHeightJumps;
+        if (jumpHeight > flagHeight) {
+            minNumberOfJumps = flagHeight;
+        } else if (jumpHeight == flagHeight) {
+            minNumberOfJumps = 1;
+        } else {
+            numJumpHeightJumps = (int) (flagHeight / jumpHeight);
+            minNumberOfJumps = numJumpHeightJumps + (flagHeight - (jumpHeight * numJumpHeightJumps));
+        }
+        return minNumberOfJumps;
     }
 }
